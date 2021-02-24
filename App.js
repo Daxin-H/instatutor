7dc3a42d2ba94972c7ef60d3f332f0dd261b2291
dorothyxu <xuzhengwen23@gmail.com>
2021-02-25 02:08:51 +0800

    Created a basic Welcome page

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+import Landing from '.components/layout/Landing';
+import Navbar from '.components/layout/Navbar';
+import './App.css';
+
+
+const App = () => 
+    <Fragment>
+      <Navbar />
+      <Landing />
+    </Fragment>
+
+
+export default App;
