ab5c815c226be01b8776b0a48c7e1677a29fffef
savula13 <sai.avula13@gmail.com>
2021-02-24 16:33:15 -0500

    Fixed users.js bugs
    Fixed typo in errors array, removed normalize from gravatar which was causing errors, added missing bycrpt requires statement

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const { check, validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 
 
@@ -14,11 +15,12 @@ router.get('/', (req, res) => res.send('Users route'));
 router.post('/', [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
-    check('password', 'PLease enter a password with 6 or more characters'
+    check('password', 'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
 ],
+
     async (req, res) => {
-        const error = validationResult(req);
+        const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ error: errors.array() });
         }
@@ -30,6 +32,7 @@ router.post('/', [
                 res.status(400).json({ errors: [{ msg: 'User already exists' }] });
             }
             // Get user gravatar
+            /*
             const avatar = normalize(
                 gravatar.url(email, {
                     s: '200',
@@ -37,6 +40,15 @@ router.post('/', [
                     d: 'mm'
                 }),
                 { forceHttps: true });
+            */
+           //without normalize
+           const avatar =
+            gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            })
+
             user = new User({
                 name,
                 email,
@@ -56,5 +68,6 @@ router.post('/', [
             res.status(500).send('Server error');
         }
     }
+    
 );
 module.exports = router; 
