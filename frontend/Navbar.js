82e467009d1e4d186995f7616ee4b0a51fd8df68
dorothyxu <xuzhengwen23@gmail.com>
2021-03-03 20:29:44 +0800

    Created login and register page

--- frontend/Navbar.js
+++ frontend/Navbar.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="navbar bg-dark">
       <h1>
-        <a href="index.html"><i className="fas fa-code"></i> Instantutor</a>
+        <Link to="/">
+          <i className="fas fa-code"></i> Instantutor
+        </Link>
       </h1>
       <ul>
-        <li><a href="register.html">Register</a></li>
-        <li><a href="login.html">Login</a></li>
+        <li>
+          <a href="!#">Profiles</a>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
       </ul>
     </nav>
     )
