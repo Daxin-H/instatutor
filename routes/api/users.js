52afc1040c0799edd179bdc34606971affd5c985
Setsurinne <63473855+Setsurinne@users.noreply.github.com>
2021-03-06 20:31:02 -0500

    Modified Model: User
    To add role (student/tutor)

--- routes/api/users.js
+++ routes/api/users.js
@@ -15,12 +15,15 @@ const User = require('../../models/User');
 // @access  Public
 router.post('/',
     [
-        check('name', 'Name is required').not()
+        check('name', 'Name is required')
+            .not()
             .isEmpty(),
-        check('email', 'Please include a valid email').isEmail(),
-        check('password', 'Please enter a password with 6 or more characters'
-        ).isLength({ min: 6 }),
-        check('role', 'Role is required').not()
+        check('email', 'Please include a valid email')
+            .isEmail(),
+        check('password', 'Please enter a password with 6 or more characters')
+            .isLength({ min: 6 }),
+        check('role', 'Role is required')
+            .not()
             .isEmpty()
     ],
 
