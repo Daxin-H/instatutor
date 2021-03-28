9003ce3c1126741632c8917232a64016750b87cb
Setsurinne <xjx1525392406@gmail.com>
2021-03-28 17:09:03 -0400

    Small update on icon of searchbar

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -7,7 +7,9 @@ const DashboardActions = () => {
 
         <div class="dash-buttons">
 
-            <Link to="/Search" class="btn btn-light"> Search bar </Link>
+            <Link to="/Search" class="btn btn-light"
+            ><i class= "fas fa-search text-primary"></i> Search bar </Link>
+
             <Link to="/edit-profile" class="btn btn-light"
             ><i class="fas fa-user-circle text-primary"></i> Edit Profile</Link>
 
